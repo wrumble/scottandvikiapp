b645dcd032ab195e9467fdff2c0800f16479e7ab
Wayne Rumble <wayne@novoda.com>
2018-04-17 21:44:04 +0100

    Title and background added

---
+++ lib/Localisations.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show SynchronousFuture;
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class Localize {
+  Localize(this.locale);
+
+  final Locale locale;
+
+  static Localize of(BuildContext context) {
+    return Localizations.of<Localize>(context, Localize);
+  }
+
+  static Map<String, Map<String, String>> _localizedValues = {
+    'en': {
+      'appTitle': 'Scott and Viki',
+      'welcomeMessage': 'Welcome to Scott and Vikis wedding App'
+    }
+  };
+
+  String get appTitle {
+    return _localizedValues[locale.languageCode]['appTitle'];
+  }
+  String get welcomeMessage {
+    return _localizedValues[locale.languageCode]['welcomeMessage'];
+  }
+}
+
+class LocalizeDelegate extends LocalizationsDelegate<Localize> {
+  const LocalizeDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['en'].contains(locale.languageCode);
+
+  @override
+  Future<Localize> load(Locale locale) {
+    // Returning a SynchronousFuture here because an async "load" operation
+    // isn't needed to produce an instance of DemoLocalizations.
+    return new SynchronousFuture<Localize>(new Localize(locale));
+  }
+
+  @override
+  bool shouldReload(LocalizeDelegate old) => false;
+}
