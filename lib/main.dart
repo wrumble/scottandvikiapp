71718b20927ddd625e1cc08c181dd8a74e866c9f
Wayne Rumble <wayne@novoda.com>
2018-04-18 15:16:22 +0100

    add working image picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,73 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'Localisations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() => runApp(new App());
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      onGenerateTitle: (BuildContext context) => Localize.of(context).appTitle,
+      localizationsDelegates: [
+        const LocalizeDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', '')
+      ],
+      // Watch out: MaterialApp creates a Localizations widget
+      // with the specified delegates. DemoLocalizations.of()
+      // will only find the app's Localizations widget if its
+      // context is a child of the app.
+      home: new MyApp(),
+    );
+  }
+}
+
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return new MaterialApp(
+      onGenerateTitle: (BuildContext context) => Localize.of(context).appTitle,
+      localizationsDelegates: [
+        const LocalizeDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', '')
+      ],
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<File> _imageFile;
+
+  void _onImageButtonPressed(ImageSource source) {
+    setState(() {
+      _imageFile = ImagePicker.pickImage(source: source);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
     var statusBarHeight = MediaQuery.of(context).padding.top;
     var titleText = new Text(Localize.of(context).appTitle,
         textAlign: TextAlign.center,
@@ -31,7 +92,7 @@ class MyApp extends StatelessWidget {
               new Container(
                 margin: EdgeInsets.only(top: 10.0),
                 child: titleText
-          ),
+          )
         ],
       ),
 
@@ -39,28 +100,24 @@ class MyApp extends StatelessWidget {
 
     return new Scaffold(
       body: mainContainer,
-    );
-  }
-}
-
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      onGenerateTitle: (BuildContext context) => Localize.of(context).appTitle,
-      localizationsDelegates: [
-        const LocalizeDelegate(),
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: [
-        const Locale('en', '')
-      ],
-      // Watch out: MaterialApp creates a Localizations widget
-      // with the specified delegates. DemoLocalizations.of()
-      // will only find the app's Localizations widget if its
-      // context is a child of the app.
-      home: new MyApp(),
+      floatingActionButton: new Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          new FloatingActionButton(
+            onPressed: () => _onImageButtonPressed(ImageSource.gallery),
+            tooltip: 'Pick Image from gallery',
+            child: new Icon(Icons.photo_library),
+          ),
+          new Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: new FloatingActionButton(
+              onPressed: () => _onImageButtonPressed(ImageSource.camera),
+              tooltip: 'Take a Photo',
+              child: new Icon(Icons.camera_alt),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
