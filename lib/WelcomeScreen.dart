89d0f6b231a492d762c10e61d03a61f00ebe4f14
Wayne Rumble <wayne@novoda.com>
2018-05-09 22:15:58 +0100

    setting up textformfield

--- lib/WelcomeScreen.dart
+++ lib/WelcomeScreen.dart
@@ -2,12 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:scott_and_viki/Constants/FontNames.dart';
 import 'package:scott_and_viki/Text/TitleText.dart';
 import 'Localisations.dart';
+import 'package:flutter/services.dart';
 import 'dart:io';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'main.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
+
+  void initState() {
+    _controller.addListener(onChange);
+    _textFocus.addListener(onChange);
+  }
+
+  TextEditingController _controller = new TextEditingController();
+
+  FocusNode _textFocus = new FocusNode();
+
+  void onChange(){
+    String text = _controller.text;
+    bool hasFocus = _textFocus.hasFocus;
+    //do your text transforming
+    _controller.text = newText;
+    _controller.selection = new TextSelection(
+        baseOffset: newText.length,
+        extentOffset: newText.length
+    );
+  }
+
   Widget build(BuildContext context) {
 
     var backgroundImage = new BoxDecoration(
@@ -16,33 +38,8 @@ class WelcomeScreen extends StatelessWidget {
         fit: BoxFit.cover,
       ),
     );
+
     var titleText = new TitleText(Localize.of(context).appTitle);
-    var doneButton = new FlatButton(
-      textColor: Colors.white,
-      padding: new EdgeInsets.only(top: 16.0),
-      color: Colors.black,
-      onPressed: () => () {
-        Navigator.pushReplacement(
-          context,
-          new MaterialPageRoute(builder: (context) => HomeScreen()),
-        );
-      },
-      child: new Text('Done!',
-        style: new TextStyle(fontFamily: FontName.normalFont,
-            fontSize: 30.0,
-            color: Colors.white),
-      ),
-    );
-    double getBottomMargin() {
-      var mediaQuery = MediaQuery.of(context);
-      if (Platform.isIOS) {
-        var size = mediaQuery.size;
-        if (size.height == 812.0 || size.width == 812.0) {
-          return mediaQuery.padding.bottom;
-        }
-      }
-      return mediaQuery.padding.bottom + 8;
-    }
 
     var requestTextContainer = new Container(
         child: new Text("Please enter your first and last name.",
@@ -67,11 +64,15 @@ class WelcomeScreen extends StatelessWidget {
         )
     );
 
+
+
     var firstNameContainer = new Container(
         child: new TextFormField(
+          controller: _controller,
+          focusNode: _textFocus,
           style: new TextStyle(
             fontFamily: FontName.normalFont,
-            fontSize: 20.0,
+            fontSize: 25.0,
             color: Colors.black,
           ),
           keyboardType: TextInputType.text,
@@ -83,7 +84,7 @@ class WelcomeScreen extends StatelessWidget {
           ),
         ),
         margin: new EdgeInsets.only(top: 16.0, bottom: 8.0),
-        padding: new EdgeInsets.only(left: 8.0, top: 8.0,  right: 8.0, bottom: 8.0),
+        padding: new EdgeInsets.only(left: 8.0, right: 8.0, bottom: 16.0),
         decoration: new BoxDecoration(
             color: Colors.white,
             boxShadow: [
@@ -100,19 +101,18 @@ class WelcomeScreen extends StatelessWidget {
         child: new TextFormField(
           style: new TextStyle(
             fontFamily: FontName.normalFont,
-            fontSize: 20.0,
+            fontSize: 25.0,
             color: Colors.black,
           ),
           keyboardType: TextInputType.text,
           autofocus: true,
           decoration: new InputDecoration(
             labelText: 'Last Name',
-            fillColor: Colors.white,
-
+            fillColor: Colors.black
           ),
         ),
-        margin: new EdgeInsets.only(top: 16.0, bottom: 8.0),
-        padding: new EdgeInsets.only(left: 8.0, top: 8.0,  right: 8.0, bottom: 8.0),
+        margin: new EdgeInsets.only(top: 16.0, bottom: 16.0),
+        padding: new EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
         decoration: new BoxDecoration(
             color: Colors.white,
             boxShadow: [
@@ -125,10 +125,59 @@ class WelcomeScreen extends StatelessWidget {
         )
     );
 
+    var passwordContainer = new Container(
+        child: new TextFormField(
+          style: new TextStyle(
+            fontFamily: FontName.normalFont,
+            fontSize: 25.0,
+            color: Colors.black,
+          ),
+          keyboardType: TextInputType.text,
+          autofocus: true,
+          decoration: new InputDecoration(
+            labelText: 'Password',
+            fillColor: Colors.white,
+
+          ),
+        ),
+        margin: new EdgeInsets.only(top: 16.0, bottom: 16.0),
+        padding: new EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
+        decoration: new BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              new BoxShadow(
+                  color: Colors.black38,
+                  blurRadius: 5.0,
+                  offset: new Offset(3.0, 5.0)
+              ),
+            ]
+        )
+    );
+
+    void showHomeScreen() {
+      Navigator.pushReplacement(
+        context,
+        new MaterialPageRoute(builder: (context) => new HomeScreen()),
+      );
+    }
+
+    var doneButton = new FlatButton(
+      textColor: Colors.white,
+      padding: new EdgeInsets.only(top: 8.0, bottom: 8.0),
+      color: Colors.black,
+      onPressed: () => showHomeScreen(),
+      child: new Text('Done!',
+        style: new TextStyle(fontFamily: FontName.normalFont,
+            fontSize: 30.0,
+            color: Colors.white),
+      ),
+    );
+
     var list = [
       requestTextContainer,
       firstNameContainer,
       lastNameContainer,
+      passwordContainer,
       doneButton
     ];
 
