7bf10216c95f58451f2fd62c4aed3f7c0bb8ec4c
Wayne Rumble <wayne@novoda.com>
2018-06-19 19:57:44 +0100

    update order of the day

--- lib/OrderOfTheDay.dart
+++ lib/OrderOfTheDay.dart
@@ -32,7 +32,7 @@ class OrderOfTheDayState extends State<OrderOfTheDay> {
     var titleText = new TitleText('Order of the day', 30.0);
 
     var ceremonyContainer = new Container(
-        child: new Text("12:30 - Wedding ceremony at St Dunstan’s Church, Mayfield followed by the wedding breakfast at Juddwood Farm",
+        child: new Text("12:30 - Wedding ceremony at St Dunstan’s Church, Mayfield",
             style: new TextStyle(
                 fontFamily: 'DancingScript-Regular',
                 fontSize: 30.0,
@@ -53,8 +53,96 @@ class OrderOfTheDayState extends State<OrderOfTheDay> {
         )
     );
 
-    var receptionContainer = new Container(
-        child: new Text("19:30 - Evening reception at Juddwood Farm where there will a small buffet to keep your dancing energy up!",
+    var canapesContainer = new Container(
+        child: new Text("14:30 Drinks reception with canapes at Juddwood Farm",
+          style: new TextStyle(
+              fontFamily: 'DancingScript-Regular',
+              fontSize: 30.0,
+              color: Colors.black
+          ),
+        ),
+        margin: new EdgeInsets.all(8.0),
+        padding: new EdgeInsets.only(left: 8.0, top: 8.0, right: 4.0, bottom: 4.0),
+        decoration: new BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              new BoxShadow(
+                  color: Colors.black38,
+                  blurRadius: 5.0,
+                  offset: new Offset(3.0, 5.0)
+              ),
+            ]
+        )
+    );
+
+    var breakfastContainer = new Container(
+        child: new Text("16:15 Wedding breakfast",
+          style: new TextStyle(
+              fontFamily: 'DancingScript-Regular',
+              fontSize: 30.0,
+              color: Colors.black
+          ),
+        ),
+        margin: new EdgeInsets.all(8.0),
+        padding: new EdgeInsets.only(left: 8.0, top: 8.0, right: 4.0, bottom: 4.0),
+        decoration: new BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              new BoxShadow(
+                  color: Colors.black38,
+                  blurRadius: 5.0,
+                  offset: new Offset(3.0, 5.0)
+              ),
+            ]
+        )
+    );
+
+    var eveningContainer = new Container(
+        child: new Text("19:30 Evening reception begins",
+          style: new TextStyle(
+              fontFamily: 'DancingScript-Regular',
+              fontSize: 30.0,
+              color: Colors.black
+          ),
+        ),
+        margin: new EdgeInsets.all(8.0),
+        padding: new EdgeInsets.only(left: 8.0, top: 8.0, right: 4.0, bottom: 4.0),
+        decoration: new BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              new BoxShadow(
+                  color: Colors.black38,
+                  blurRadius: 5.0,
+                  offset: new Offset(3.0, 5.0)
+              ),
+            ]
+        )
+    );
+
+    var cakeContainer = new Container(
+        child: new Text("19:45 Cutting the cake, followed by the first dance",
+          style: new TextStyle(
+              fontFamily: 'DancingScript-Regular',
+              fontSize: 30.0,
+              color: Colors.black
+          ),
+        ),
+        margin: new EdgeInsets.all(8.0),
+        padding: new EdgeInsets.only(left: 8.0, top: 8.0, right: 4.0, bottom: 4.0),
+        decoration: new BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              new BoxShadow(
+                  color: Colors.black38,
+                  blurRadius: 5.0,
+                  offset: new Offset(3.0, 5.0)
+              ),
+            ]
+        )
+    );
+
+    var bandsContainer = new Container(
+        child: new Text("19:45 Onwards - Mikey Gray and BringYour Sisters will be playing into the late evening. A small buffet will be served to keep your dancing energy up!",
           style: new TextStyle(
               fontFamily: 'DancingScript-Regular',
               fontSize: 30.0,
@@ -76,7 +164,7 @@ class OrderOfTheDayState extends State<OrderOfTheDay> {
     );
 
     var barContainer = new Container(
-        child: new Text("00:00 - There will be a cash bar until midnight.",
+        child: new Text("00:00 A cash bar will be open throughout the evening to close at midnight",
           style: new TextStyle(
               fontFamily: 'DancingScript-Regular',
               fontSize: 30.0,
@@ -99,7 +187,11 @@ class OrderOfTheDayState extends State<OrderOfTheDay> {
 
     var listContainers = [
       ceremonyContainer,
-      receptionContainer,
+      canapesContainer,
+      breakfastContainer,
+      eveningContainer,
+      cakeContainer,
+      bandsContainer,
       barContainer
     ];
 
